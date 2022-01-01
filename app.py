1f3661ef86e52d5e81e2ad466a603aa967c3d653
Guy Marshall <guymarshall9@gmail.com>
2022-01-01 17:58:40 +0000

    Removed unnecessary import + formatted with PEP8 spacing between functions

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from urllib.request import urlopen
-import urllib
+
 
 def get_element_by_tag(url, html_tag):
     page = urlopen(url)
@@ -13,8 +13,10 @@ def get_element_by_tag(url, html_tag):
     element = html[start_index:end_index]
     return element
 
+
 def text_to_html(text):
     return text.replace(" ", "%20").lower()
+
 
 def main():
     keywords = ["programming", "software development", "coding", "python", "java", "php", "web", "ai", "machine learning"]
@@ -23,6 +25,7 @@ def main():
     for keyword in keywords:
         url = f"https://{country}.indeed.com/jobs?q={text_to_html(keyword)}&l={location}&vjk=38d83016285aee43"
         print(get_element_by_tag(url, "title"))
+
 
 if __name__ == "__main__":
     main()
